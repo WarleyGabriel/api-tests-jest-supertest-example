074f667331a72bdf90e8b13f0e5cffa6dd5738a4
Warley Gabriel da Paixão <warley.paixao@softplan.com.br>
2019-06-29 18:14:17 -0300

    Add contract validations
    Includes lib to validate contracts, and to generate fake data.

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,6 +1,11 @@
-import { assert } from 'chai';
+import jsf from 'json-schema-faker';
+import chai, { assert } from 'chai';
+import jsonSchema from 'chai-json-schema';
 import { postman } from '../services/postman';
 import echoPostmanResponse from '../data/echo.postman.response';
+import echoPostmanSchema from '../schema/echo.postman.schema';
+
+chai.use(jsonSchema);
 
 describe('My first API test.', () => {
   it('Get on Postman website.', async () => {
@@ -8,17 +13,29 @@ describe('My first API test.', () => {
     assert.equal(
       response.status,
       200,
-      'Testing the correct status',
+      'Testing the right status',
     );
-    assert.containsAllKeys(
+    assert.jsonSchema(
       response.data,
-      echoPostmanResponse,
-      'Testing the correct contract',
+      echoPostmanSchema,
+      'Testing the right contract',
     );
     assert.deepEqual(
       response.data,
       echoPostmanResponse,
-      'Testing the correct response',
+      'Testing the right response',
+    );
+  });
+
+  it('Example generating fake data.', () => {
+    const fake = jsf.generate(echoPostmanSchema);
+    assert.containsAllKeys(
+      echoPostmanResponse,
+      fake,
+    );
+    assert.notDeepEqual(
+      echoPostmanResponse,
+      fake,
     );
   });
 });
