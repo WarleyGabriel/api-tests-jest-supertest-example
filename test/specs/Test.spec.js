2f4e5768291548014c89168dc5c183f6cd166b12
Warley Gabriel da Paixão <wgabrieldapaixao@gmail.com>
2019-09-08 15:04:37 -0300

    Adiciona report awesome, supertest and chance

--- test/specs/Test.spec.js
+++ test/specs/Test.spec.js
@@ -1,25 +1,23 @@
 import jsf from 'json-schema-faker';
 import chai, { assert } from 'chai';
 import jsonSchema from 'chai-json-schema';
+import Chance from 'chance';
+import request from 'supertest';
 import * as HttpStatus from 'http-status-codes';
 import { postmanService } from '../services/Postman.service';
 import echoPostmanResponse from '../data/responses/EchoPostman.response';
 import echoPostmanSchema from '../data/schemas/EchoPostman.schema';
 
 chai.use(jsonSchema);
+const chance = new Chance();
 
 describe('My first API test.', () => {
-    it('Get on Postman website.', async () => {
+    it('Check response on Postman website.', async () => {
         const response = await postmanService.getEchoPostman();
         assert.equal(
             response.status,
             HttpStatus.OK,
             'Testing the right status',
-        );
-        assert.jsonSchema(
-            response.data,
-            echoPostmanSchema,
-            'Testing the right contract',
         );
         assert.deepEqual(
             response.data,
@@ -28,9 +26,30 @@ describe('My first API test.', () => {
         );
     });
 
+    it('Check contract on Postman website.', async () => {
+        const response = await postmanService.getEchoPostman();
+        assert.jsonSchema(
+            response.data,
+            echoPostmanSchema,
+            'Testing the right contract',
+        );
+    });
+
     it('Example generating fake data.', () => {
         const fake = jsf.generate(echoPostmanSchema);
         assert.containsAllKeys(echoPostmanResponse, fake);
         assert.notDeepEqual(echoPostmanResponse, fake);
+    });
+
+    it('Just an example with "chance".', () => {
+        assert.isString(chance.cpf());
+    });
+
+    it('Just an example with "supertest".', (done) => {
+        request('https://postman-echo.com')
+            .get('/get')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(HttpStatus.OK, done);
     });
 });
